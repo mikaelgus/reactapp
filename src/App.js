5e1db1f888a804725401f728887083137dbd456a
Mikael Gustafsson <mikaelgustafsson@Mikaels-MacBook-Pro.local>
2022-03-21 10:05:57 +0200

    some navigations

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {Route, Routes, Router} from 'react-router-dom';
 import './App.css';
-import MediaTable from './components/MediaTable';
+// import MediaTable from './components/MediaTable';
+import Nav from './components/Nav';
+import Home from './views/Home';
+import Profile from './views/Profile';
 
 const App = () => {
-  return <MediaTable />;
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="profile/" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
